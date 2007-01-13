9a4a9d927c8a511cdc712068c48ffe78a40d3a52
keiichi <keiichi@mobilegravity.sfc.wide.ad.jp>
2007-01-13 00:45:52 +0000

    Initial revision

---
+++ src/sys/arch/zaurus/include/types.h
@@ -0,0 +1,11 @@
+/*	$NetBSD: types.h,v 1.2 2007/01/08 20:56:34 peter Exp $	*/
+
+#ifndef _ZAURUS_TYPES_H_
+#define	_ZAURUS_TYPES_H_
+
+#include <arm/arm32/types.h>
+
+#define	__HAVE_DEVICE_REGISTER
+#define	__HAVE_GENERIC_SOFT_INTERRUPTS
+
+#endif	/* _ZAURUS_TYPES_H_ */
